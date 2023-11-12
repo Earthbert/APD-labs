817349cbfeec617805d5fb1cf67fab1084149895
Earthbert <daraban_timotei@tutanota.com>
2023-11-12 13:31:30 +0200

    Add lab6

---
+++ lab06/src/multipleProducersMultipleConsumers/Buffer.java
@@ -0,0 +1,24 @@
+package multipleProducersMultipleConsumers;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Buffer {
+	ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(1);
+
+	void put(int value) {
+		try {
+			queue.put(value);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	int get() {
+		try {
+			return queue.take();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+}
